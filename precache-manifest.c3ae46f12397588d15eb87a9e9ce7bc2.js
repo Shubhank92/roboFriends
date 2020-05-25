6ab821ee7c674e605399a7ad73afa504222e458c
Shubhank <shubhankgaba92@gmail.com>
2020-05-25 20:26:06 +0530

    Updates

---
+++ precache-manifest.c3ae46f12397588d15eb87a9e9ce7bc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "325eb82587da9d77212151d12134ec51",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d9f48f9bd38991c063c4",
+    "url": "/roboFriends/static/css/main.86166839.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/roboFriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboFriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f48f9bd38991c063c4",
+    "url": "/roboFriends/static/js/main.55ec80d7.chunk.js"
+  },
+  {
+    "revision": "db7f08724344aac129ee",
+    "url": "/roboFriends/static/js/runtime-main.ed696bc5.js"
+  },
+  {
+    "revision": "e6108fee300bcc7274806cec4c44f52d",
+    "url": "/roboFriends/static/media/HACKED_Title.e6108fee.woff"
+  }
+]);
